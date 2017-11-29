5edae9f669bae37adb4dd73a319beba05244f872
simon <who8023me@gmail.com>
2017-11-29 17:58:39 +0800

    git try-finally usaging

---
+++ finally.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# Filename: finally.py
+import time
+try:
+	f= file('finally.py')
+	while True:#our usual file-reading idiom
+		line=f.readline()
+		if len(line)==0:
+			break
+		time.sleep(2)
+		print line,
+finally:
+		f.close()
+		print 'Cleaning up...closed the file'
