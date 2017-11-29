b9d5be6d7bf4c452ee91b3312545dea3615eb2e2
simon <who8023me@gmail.com>
2017-11-29 17:38:04 +0800

    python try except usaging

---
+++ try_except.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#Filename:try_except.py
+import sys
+try:
+	s=raw_input('Enter something-->')
+except EOFError:
+	print '\nWhy did you do an EOF on me?'
+	sys.exit()#exit the program
+except:
+	print '\n Some error/exception occured.'
+	#here,we are not exiting the program
+print 'Done'
